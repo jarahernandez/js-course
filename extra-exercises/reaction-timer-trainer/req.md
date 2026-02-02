d14ba5d8bb52fef6afcaccc61116ec4d964ab9ec
jarahernandez <ejarahernandez1@gmail.com>
2026-02-02 13:09:43 -0500

    finish timer trainer project

--- extra-exercises/reaction-timer-trainer/req.md
+++ extra-exercises/reaction-timer-trainer/req.md
@@ -1,3 +1,5 @@
+# User story
+
 - User opens the page and is met with a timer which is centered in the page (eg 0.0s) and another timer (smaller) under it with the 'Best Time' (when the user first enters the page both are set to 0.0s)
 - Page will have a neutral color as the background
 - Underneath the timer there is a button ('Start')
@@ -8,3 +10,9 @@
 - This will be the cue for the user to click the 'Stop' button as fast as possible
 - After user clicks on the 'Stop' button, the page will change the timer from 0.0s to whatever time it took the user to react
 - If best time, it will also change the smaller timer under the main one to store the best reaction time.
+
+# Improvements to make
+- Refactor to a single gameState variable
+- Replace all toggle() calls with explicit state-setting
+- Add keyboard support (spacebar to stop)
+- Do a code review with your friend using your pairing guide
--- extra-exercises/reaction-timer-trainer/script.js
+++ extra-exercises/reaction-timer-trainer/script.js
@@ -1,32 +1,75 @@
 'use strict';
 
 const start = document.querySelector('.start');
+const stop = document.querySelector('.stop')
+const reset = document.querySelector('.reset')
 const body = document.querySelector('body');
 const bodyCL = body.classList;
+const instructionCL = document.querySelector('.instruction').classList;
+const currentTimeE = document.querySelector('.current-time');
+const recordTimeE = document.querySelector('.record-time');
+let earlyStart = true;
+let timeOutId = 0;
+let startTime = 0;
+let endTime = 0;
 
-// const timer1 = () => {
-//     console.time('timer')
-//     setTimeout(() => {
-//         console.log('timer:');
-//     }, 3000);
-//     console.time('timer')
-//     return endTime - startTime;
-// };
-
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-async function startHandler() {
-    await sleep(3000);
-    bodyCL.toggle('waiting');
-    bodyCL.toggle('ready');
-    const startTime = performance.now();
-    console.log(`in async function: ${startTime}`);
-    return startTime;
-}
+const randomDelayGen = () => Math.floor(Math.random() * 5000) + 1000;
+let randomDelay = randomDelayGen();
 
 start.addEventListener('click', () => {
     bodyCL.toggle('idle');
     bodyCL.toggle('waiting');
-    const startTime = startHandler();
-    console.log(startTime);
+    timeOutId = setTimeout(() => {
+        bodyCL.toggle('waiting')
+        bodyCL.toggle('ready')
+        earlyStart = false;
+        startTime = performance.now();
+    }, randomDelay)
+    start.disabled = true;
+    stop.disabled = false;
 });
+
+stop.addEventListener('click', () => {
+    if (earlyStart) {
+        clearTimeout(timeOutId);
+        instructionCL.toggle('hidden');
+        bodyCL.contains('ready') ? bodyCL.toggle('ready') : '';
+        bodyCL.contains('waiting') ? bodyCL.toggle('waiting') : '';
+        bodyCL.toggle('finished');
+        stop.disabled = true;
+    } else {
+        endTime = performance.now();
+        bodyCL.contains('ready') ? bodyCL.toggle('ready') : '';
+        bodyCL.contains('waiting') ? bodyCL.toggle('waiting') : '';
+        bodyCL.toggle('finished');
+        stop.disabled = true;
+        console.log(`Start time: ${startTime}ms\nEnd Time: ${endTime}ms`);
+        let reactionTimeInSecs = (endTime-startTime)/1000;
+        reactionTimeInSecs = reactionTimeInSecs.toFixed(3);
+        console.log(`Reaction time: ${reactionTimeInSecs}s`);
+        let recordTimeNum = Number(recordTimeE.textContent);
+        currentTimeE.textContent = reactionTimeInSecs;
+
+        if (recordTimeNum === 0) {
+            console.log('record is 0');
+            recordTimeE.textContent = reactionTimeInSecs;
+            return;
+        }
+        recordTimeE.textContent = reactionTimeInSecs < recordTimeNum ? reactionTimeInSecs : recordTimeNum;
+    }
+})
+
+reset.addEventListener('click', () => {
+    body.setAttribute('class', '');
+    bodyCL.add('idle');
+    if(!instructionCL.contains('hidden')){
+        instructionCL.toggle('hidden');
+    }
+    start.disabled = false;
+    stop.disabled = true;
+    currentTimeE.textContent = '0.0'
+    startTime = 0;
+    endTime = 0;
+    earlyStart = true;
+    randomDelay = randomDelayGen();
+})
