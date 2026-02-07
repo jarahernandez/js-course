52e5d28d16df257a1a0fb07adaa8319b2b8af24d
jarahernandez <ejarahernandez1@gmail.com>
2026-02-07 08:11:42 -0500

    implement winning condition and fix total score counting error. dice images were added

--- dom-and-events/pig-game/script.js
+++ dom-and-events/pig-game/script.js
@@ -9,6 +9,8 @@ const currentScoreP2 = document.querySelector('.current-score-p2');
 // Variables
 let playerTurn = true; // true for player#1 - false for player#2
 let currentScore = 0;
+let playerScore1 = Number(totalScoreP1.textContent);
+let playerScore2 = Number(totalScoreP2.textContent);
 
 // Buttons
 const newGameBtn = document.querySelector('.new-game');
@@ -21,7 +23,12 @@ const playerHeader2 = document.querySelector('.p2-header');
 
 // Dice
 const dice = document.querySelector('.dice');
+const diceImg = document.querySelector('.dice-img');
 
+// Winner
+const winner = document.querySelector('.winner');
+
+// ---------------------- Game Logic ----------------------
 // Rolling dice logic
 const rollDice = () => {
     return Math.floor(Math.random() * 6) + 1;
@@ -30,10 +37,11 @@ const rollDice = () => {
 // Rolling dice event
 rollDiceBtn.addEventListener('click', () => {
     const diceValue = rollDice();
+    diceImg.src = `static/${diceValue}.png`;
     dice.textContent = diceValue;
     if (diceValue === 1) {
         hold();
-        // playerTurn = !playerTurn;
+        changeActivePlayer();
         return;
     }
     currentScore += diceValue;
@@ -42,14 +50,23 @@ rollDiceBtn.addEventListener('click', () => {
         : (currentScoreP2.textContent = currentScore);
 });
 
-// Hold functionality
+// Hold event
 holdBtn.addEventListener('click', () => {
-    // Set current value to total value
-    playerTurn
-        ? (totalScoreP1.textContent = currentScoreP1.textContent)
-        : (totalScoreP2.textContent = currentScoreP2.textContent);
-
+    // Add current value to total value
+    if (playerTurn) {
+        playerScore1 += currentScore;
+        totalScoreP1.textContent = playerScore1;
+    } else {
+        playerScore2 += currentScore;
+        totalScoreP2.textContent = playerScore2;
+    }
     hold();
+    // Check for winner
+    if (checkWinner()) {
+        checkWinner();
+        return;
+    }
+    changeActivePlayer();
 });
 
 // Funtion to reset current score for player that just holded or for player that rolled a 1
@@ -58,12 +75,32 @@ const hold = () => {
     playerTurn
         ? (currentScoreP1.textContent = currentScore)
         : (currentScoreP2.textContent = currentScore);
-    playerTurn = !playerTurn;
-    activePlayer();
 };
 
 // Change visual on active player
-const activePlayer = () => {
+const changeActivePlayer = () => {
+    playerTurn = !playerTurn;
     playerHeader1.classList.toggle('active-player');
     playerHeader2.classList.toggle('active-player');
+};
+
+// Win condition
+const checkWinner = () => {
+    if (playerTurn) {
+        if (playerScore1 >= 100) {
+            winner.classList.remove('hidden');
+            winner.textContent = 'WINNER: PLAYER 1';
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (playerScore2 >= 100) {
+            winner.classList.remove('hidden');
+            winner.textContent = 'WINNER: PLAYER 2';
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
