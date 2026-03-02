e3e56215374b9caa7b4464994fc01e9e4e0c1361
jarahernandez <ejarahernandez1@gmail.com>
2026-03-02 15:18:12 -0500

    Start working on bonus challenges (working with strings)

---
+++ data-structures-modern-operators-and-strings/challenges/string-methods-practice.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// String Methods Practice
+
+const flights =
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const flightsArr = flights.split('+');
+console.log(flightsArr);
+
+for (const [i, flight] of flightsArr.entries()) {
+    const flight1 = flight.split('_').join(' ').split(';').join(' ');
+    console.log(flight1);
+}
